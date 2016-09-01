ba9a1bcbc99317bdb2ba05155ca5fab75bc47a63
Cem Ozturan <cozturan@findmypast.com>
2016-09-01 12:31:39 +0100

    adding static directories to serve public files (styles, third party js, etc.) and views

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ var app = express();
 // The port that Express listens on our machine. let's just pick 5000.
 var port = 5000;
 
+// app.use() sets up some middleware. Whatever we do is app.use() is executed by Express first before it does anything else.
+// The reason we are doing is to be able to serve public files from a static directory.
+// For example, to get all the css files (styles.css, bootstrap.min.css, etc), Express would need individual routes, which would
+// be ridicilous to set up. By setting a static directory below, we are telling Express to check there first before anything else.
+// For example, if a request comes for 'css/styles.css', Express will first look inside the 'public' folder and serve that static file up if finds it.
+// After that, it starts to do the routes.
+// Now, we can go to http://localhost:5000/css/styles.css and get a successful response.
+app.use(express.static('public'));
+
+// Setting up a second static directory to serve the views. Express will first check the public folder, and then src/views,
+// and if it still cannot find what it wants, then it'll check our app.get in our routes.
+app.use(express.static('src/views'));
+
 // '/' is the home route, so in this case localhost:5000
 // Also, we pass Express a function that tells it what to do when this route is hit.
 // The function takes in two parameters; request (info that is coming from the browser, eg., if it is a post it'll be the whole body)
