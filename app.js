97d433598e59f3c52befaa695c1a7386cd59a272
Cem Ozturan <cozturan@findmypast.com>
2016-09-01 12:08:42 +0100

    adding simple routing to home page and /books

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ var app = express();
 // The port that Express listens on our machine. let's just pick 5000.
 var port = 5000;
 
+// '/' is the home route, so in this case localhost:5000
+// Also, we pass Express a function that tells it what to do when this route is hit.
+// The function takes in two parameters; request (info that is coming from the browser, eg., if it is a post it'll be the whole body)
+// and response (this is what we are going to be dealing with).
+// So, this is Express taking a request from the browser, and sending it something back.
+app.get('/', function(req, res) {
+  res.send('Hello world');
+});
+
+app.get('/books', function(req, res) {
+  res.send('Hello books');
+});
+
 // We fire the Express instance off.
 // Takes a couple of parameters; a port and a callback.
 app.listen(port, function(err) {
