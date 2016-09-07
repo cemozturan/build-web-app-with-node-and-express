6eea05e24c265a5c699e481aeae281744da3bcb5
Cem Ozturan <cozturan@findmypast.com>
2016-09-07 17:06:51 +0100

    hooking up mongodb to the app, inserting books into the db /admin/addBooks now returns us the books that we insert into the db

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,11 +1,64 @@
 var express = require('express');
 var adminRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
+
+// Create the list of books
+var books = [
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Tolstoy',
+    read: false
+  },
+  {
+    title: 'C# in Depth',
+    genre: 'Software',
+    author: 'Jon Skeet',
+    read: false
+  },
+  {
+    title: 'Madonna in Fur Coat',
+    genre: 'Romance',
+    author: 'Sabahattin Ali',
+    read: true
+  },
+  {
+    title: 'Master and Margarita',
+    genre: 'Fiction',
+    author: 'N. Bulgakov',
+    read: false
+  },
+  {
+    title: 'Shredded Wheat',
+    genre: 'Food',
+    author: 'Ms. Nestle',
+    read: true
+  },
+];
 
 var router = function(nav) {
 
   adminRouter.route('/addBooks')
     .get(function(req, res) {
-      res.send('inserting books');
+      // 27017 is the default port number for mongoDB, can be changed.
+      // In cmd, run "mongo" to be able to write mongo commands,
+      // then "show dbs" to see the available databases
+      var url = 'mongodb://localhost:27017/node-express-course';
+
+      mongodb.connect(url, function(err, db) {
+        // You can think of MongoDB collections as tables in SQL.
+        // They are actually different but they are close enough to wrap your head around by thinking that way.
+        // Unlike SQL Server, you can't do a query to multiple collections at one time.
+        // There is no joining, no selecting across.
+        var collection = db.collection('books');
+
+        // results are the same array we passed in but with some mongoDB properties
+        collection.insertMany(books, function(err, results) {
+          res.send(results);
+          db.close();
+        });
+      });
+      // calling db.close() here won't work because the insertMany is async, and we should not close the connection until it is completed.
     });
 
   return adminRouter;
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,39 +2,6 @@ var express = require('express');
 var bookRouter = express.Router();
 
 var router = function(nav) {
-  // Create the list of books
-  var books = [
-    {
-      title: 'War and Peace',
-      genre: 'Historical Fiction',
-      author: 'Lev Tolstoy',
-      read: false
-    },
-    {
-      title: 'C# in Depth',
-      genre: 'Software',
-      author: 'Jon Skeet',
-      read: false
-    },
-    {
-      title: 'Madonna in Fur Coat',
-      genre: 'Romance',
-      author: 'Sabahattin Ali',
-      read: true
-    },
-    {
-      title: 'Master and Margarita',
-      genre: 'Fiction',
-      author: 'N. Bulgakov',
-      read: false
-    },
-    {
-      title: 'Shredded Wheat',
-      genre: 'Food',
-      author: 'Ms. Nestle',
-      read: true
-    },
-  ];
 
   // Set the book router up. In addition to .get(), you can setup all other HTTP verbs at once like .get().post().put().etc...
   bookRouter.route('/') // /books/
