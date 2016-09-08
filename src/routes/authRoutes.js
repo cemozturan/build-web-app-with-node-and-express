d3a531d915d148bc79bae92baf25b0ee78ca01d2
Cem Ozturan <cozturan@findmypast.com>
2016-09-08 19:00:46 +0100

    Implemented the sign up user functionality, now a new user is inserted into the DB

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -8,20 +8,56 @@ var router = function() {
       // This prints the request body which is in JSON format because of the bodyParser we are using in app.js.
       // If we didn't have app.use(bodyParser) in app.js, req.body would be undefined
       // Now, it prints something like { userName: 'cem', password: 'dummypassword' }
+      // The property names "userName/password" come from the input controls' names on the UI (index.ejs).
       console.log(req.body);
-      /*
-        When passport does its initialisation phase (passport.initialize and passport.session), it
-        adds some things to request messages, and one of them is the "login" function. What it does is to tell passport
-        that this user is ready to sign in. During a regular sign in, where an existing user signs in with their username/password
-        we wouldn't call this as the authentication would deal with it. But here, we are signing up a new user, and we
-        wouldn't want them to go and login again just after signing up, so we call login explicitly.
-      */
-      // req.body is expected to be the user here
-      req.login(req.body, function() {
-        // So, this puts the passed in user (req.body here) into session.
-        // The passport does its serializeUser and deserializeUser stuff and makes that work.
-        // redirect the user to the profile page
-        res.redirect('/auth/profile');
+      var url = 'mongodb://localhost:27017/node-express-course';
+      mongodb.connect(url, function(err, db) {
+        // Even if the "users" collection doesn't exist,
+        // Mongo will create it automatically the first time we insert into this collection.
+        var collection = db.collection('users');
+        var user = {
+          username: req.body.userName,
+          password: req.body.password
+        };
+        // Here, we'd normally do a select first to see if this username is already in our DB, and throw error if so.
+        // But for now, we'll simply insert this user into our DB.
+        collection.insert(user, function(err, results) {
+          /*
+            When passport does its initialisation phase (passport.initialize and passport.session), it
+            adds some things to request messages, and one of them is the "login" function. What it does is to tell passport
+            that this user is ready to sign in. During a regular sign in, where an existing user signs in with their username/password
+            we wouldn't call this as the authentication would deal with it. But here, we are signing up a new user, and we
+            wouldn't want them to go and login again just after signing up, so we call login explicitly.
+          */
+          /*
+           "results" here is something like:
+          {
+            result: {
+              ok: 1,
+              n: 1
+            },
+            ops: [
+              {
+                username: "cemozturan",
+                password: "fmp-course",
+                _id: "57d1a5cf8b83fdceb803d851"
+              }
+            ],
+            insertedCount: 1,
+            insertedIds: [
+              "57d1a5cf8b83fdceb803d851"
+            ]
+          }
+
+          So, we want results.ops[0]
+          */
+          req.login(results.ops[0], function() {
+            // So, this puts the passed in user (results.ops[0] here) into session.
+            // The passport does its serializeUser and deserializeUser stuff and makes that work.
+            // redirect the user to the profile page
+            res.redirect('/auth/profile');
+          });
+        });
       });
     });
 
