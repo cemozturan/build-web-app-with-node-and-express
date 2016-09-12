5311be41b6ac88d5a8616fdb61c4e40c274bed7a
Cem Ozturan <cozturan@findmypast.com>
2016-09-12 11:54:38 +0100

    pulling logic out of bookRoutes into a controller

---
+++ src/controllers/bookController.js
@@ -0,0 +1,58 @@
+var mongodb = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+
+var bookController = function(bookService, nav) {
+
+  var authoriseCheck = function(req, res, next){
+      if (!req.user) {
+        res.redirect('/');
+      }
+      next();
+  };
+
+  var getIndex = function(req, res) {
+    var url = 'mongodb://localhost:27017/node-express-course';
+
+    mongodb.connect(url, function(err, db) {
+      var collection = db.collection('books');
+      // "find" would take in an object that represents a query, but since we want everything in this case, we pass in {}
+      // we'll use a query when we select a single item in /book/:id
+      collection.find({}).toArray(
+        function(err, results) {
+          res.render('books', {
+            title: 'Hello from BOOKS',
+            nav: nav,
+            books: results
+          });
+        });
+      });
+  };
+
+  var getById = function(req, res) {
+    var id = new ObjectId(req.params.id);
+    var url = 'mongodb://localhost:27017/node-express-course';
+
+    mongodb.connect(url, function(err, db) {
+      var collection = db.collection('books');
+
+      collection.findOne({_id: id}, function(err, results) {
+        // The first argument of render is the view under /src/views,
+        // which gets set in app.js by app.set('views', './src/views');
+        res.render('book', {
+          title: 'Hello from BOOKS',
+          nav: nav,
+          book: results
+        });
+      });
+    });
+  };
+
+  return {
+    getIndex: getIndex,
+    getById: getById,
+    authoriseCheck: authoriseCheck
+  };
+
+};
+
+module.exports = bookController;
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,59 +1,22 @@
 var express = require('express');
 var bookRouter = express.Router();
-var mongodb = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectID;
 
 var router = function(nav) {
 
+  // Get the book controller
+  var bookController = require('../controllers/bookController.js')(null, nav);
+
   // Authorize all the book routes, so if there is no user in session we get redirected to the login page
-  bookRouter.use(function(req, res, next) {
-    if (!req.user) {
-      res.redirect('/');
-    }
-    next();
-  });
+  bookRouter.use(bookController.authoriseCheck);
 
   // Set the book router up. In addition to .get(), you can setup all other HTTP verbs at once like .get().post().put().etc...
   bookRouter.route('/') // /books/
-    .get(function(req, res) {
-      var url = 'mongodb://localhost:27017/node-express-course';
-
-      mongodb.connect(url, function(err, db) {
-        var collection = db.collection('books');
-        // "find" would take in an object that represents a query, but since we want everything in this case, we pass in {}
-        // we'll use a query when we select a single item in /book/:id
-        collection.find({}).toArray(
-          function(err, results) {
-            res.render('books', {
-              title: 'Hello from BOOKS',
-              nav: nav,
-              books: results
-            });
-          });
-        });
-    });
+    .get(bookController.getIndex);
 
   // Whatever is beyond the slash, Express is going to give us as a property within the request parameters.
   // So, in this case, we can do req.params.id to get the id from the URL.
   bookRouter.route('/:id') // /books/1
-    .get(function(req, res) {
-      var id = new ObjectId(req.params.id);
-      var url = 'mongodb://localhost:27017/node-express-course';
-
-      mongodb.connect(url, function(err, db) {
-        var collection = db.collection('books');
-
-        collection.findOne({_id: id}, function(err, results) {
-          // The first argument of render is the view under /src/views,
-          // which gets set in app.js by app.set('views', './src/views');
-          res.render('book', {
-            title: 'Hello from BOOKS',
-            nav: nav,
-            book: results
-          });
-        });
-      });
-    });
+    .get(bookController.getById);
 
   return bookRouter;
 };
